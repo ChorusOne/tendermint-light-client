630de6a3dfbf00a296dbe8ddfdb22d5f1b096957
Joe Bowman <joe@chorus.one>
2020-06-04 19:23:31 +0100

    make public

--- src/lib.rs
+++ src/lib.rs
@@ -7,15 +7,15 @@ pub mod verification;
 #[macro_use]
 extern crate serde_derive;
 
-pub mod block {
+pub(crate) mod block {
     pub use crate::types::block::{height::Height, id::Id, commit::Commit, header::Header, signed_header::SignedHeader};
 }
 
-pub mod time {
+pub(crate) mod time {
     pub use crate::types::time::Time;
 }
 
-pub mod validator {
+pub(crate) mod validator {
     pub use crate::types::validator::{Set,Info};
 }
 
