3d391dc3cb7f8c213f40a8dc69493207aab99172
Parth Desai <desaiparth08@gmail.com>
2020-06-05 07:38:13 +0530

    exposing types and removing duplication

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,38 @@ mod errors;
 mod merkle_tree;
 mod serialization;
 mod types;
-pub mod verification;
+mod verification;
 
 #[macro_use]
 extern crate serde_derive;
 
-pub(crate) mod block {
-    pub use crate::types::block::{height::Height, id::Id, commit::Commit, header::Header, signed_header::SignedHeader};
-}
+/// Types required to construct call to verification functionality
+// Concrete header
+pub use types::block::header::Header as LightHeader;
+// Concrete signed header
+pub use types::block::commit::LightSignedHeader;
+// Generic signed header
+pub use types::block::commit::SignedHeader;
+// Converts a LightSignedHeader to Generic signed header
+pub use types::block::commit::convert_to_signed_header;
+// Trusted state data types
+pub use types::trusted::TrustThresholdFraction;
+pub use types::trusted::TrustedState;
+// Validator data types
+pub use types::validator::Info as LightValidatorInfo;
+pub use types::validator::Set as LightValidatorSet;
 
-pub(crate) mod time {
-    pub use crate::types::time::Time;
-}
+// Function to call to validate a header
+pub use verification::verify_single;
 
-pub(crate) mod validator {
-    pub use crate::types::validator::{Set,Info};
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+/// Traits inherited by some of the exposed types
+pub mod traits {
+    // Validator set trait implemented by LightValidatorSet
+    pub use super::types::traits::validator_set::ValidatorSet;
+    // Header trait implemented by LightHeader
+    pub use super::types::block::traits::header::Header;
+    // TrustThreshold trait implemented by TrustThresholdFraction
+    pub use super::types::traits::trusted::TrustThreshold;
+    // Provable commit trait implemented by LightSignedHeader
+    pub use super::types::block::traits::commit::ProvableCommit;
 }
