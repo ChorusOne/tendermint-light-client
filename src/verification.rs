ff4427879abe47a2c7fb11b69e24dd9a60a218d5
Parth Desai <desaiparth08@gmail.com>
2020-06-05 11:28:32 +0530

    add verification tests

--- src/verification.rs
+++ src/verification.rs
@@ -251,3 +251,293 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::types::block::traits::header::Header;
+    use crate::types::mocks::{fixed_hash, MockCommit, MockHeader, MockSignedHeader, MockValSet};
+    use crate::types::traits::validator_set::ValidatorSet;
+    use crate::verification::{is_within_trust_period, verify_single_inner};
+    use crate::{TrustThresholdFraction, TrustedState};
+    use std::time::{Duration, SystemTime};
+
+    type MockState = TrustedState<MockCommit, MockHeader>;
+
+    #[derive(Clone)]
+    struct ValsAndCommit {
+        vals_vec: Vec<usize>,
+        commit_vec: Vec<usize>,
+    }
+
+    impl ValsAndCommit {
+        pub fn new(vals_vec: Vec<usize>, commit_vec: Vec<usize>) -> ValsAndCommit {
+            ValsAndCommit {
+                vals_vec,
+                commit_vec,
+            }
+        }
+    }
+
+    // create the next state with the given vals and commit.
+    fn next_state(vals_and_commit: ValsAndCommit) -> (MockSignedHeader, MockValSet, MockValSet) {
+        let time = init_time() + Duration::new(10, 0);
+        let height = 10;
+        let vals = MockValSet::new(vals_and_commit.vals_vec);
+        let next_vals = vals.clone();
+        let header = MockHeader::new(height, time, vals.hash(), next_vals.hash());
+        let commit = MockCommit::new(header.hash(), vals_and_commit.commit_vec);
+        (MockSignedHeader::new(commit, header), vals, next_vals)
+    }
+
+    // start all blockchains from here ...
+    fn init_time() -> SystemTime {
+        SystemTime::UNIX_EPOCH
+    }
+
+    // create an initial trusted state from the given vals
+    fn init_trusted_state(
+        vals_and_commit_vec: ValsAndCommit,
+        next_vals_vec: Vec<usize>,
+        height: u64,
+    ) -> MockState {
+        // time has to be increasing:
+        let time = init_time() + Duration::new(height * 2, 0);
+        let vals = MockValSet::new(vals_and_commit_vec.vals_vec);
+        let next_vals = MockValSet::new(next_vals_vec);
+        let header = MockHeader::new(height, time, vals.hash(), next_vals.hash());
+        let commit = MockCommit::new(header.hash(), vals_and_commit_vec.commit_vec);
+        let sh = MockSignedHeader::new(commit, header);
+        MockState::new(sh, vals)
+    }
+
+    // make a state with the given vals and commit and ensure we get the expected error kind.
+    fn assert_single_err(
+        ts: &TrustedState<MockCommit, MockHeader>,
+        vals_and_commit: ValsAndCommit,
+        err_str: String,
+    ) {
+        let (un_sh, un_vals, un_next_vals) = next_state(vals_and_commit);
+        let result = verify_single_inner(
+            ts,
+            &un_sh,
+            &un_vals,
+            &un_next_vals,
+            TrustThresholdFraction::default(),
+        );
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().to_string(), err_str);
+    }
+
+    // make a state with the given vals and commit and ensure we get no error.
+    fn assert_single_ok(ts: &MockState, vals_and_commit: ValsAndCommit) {
+        let (un_sh, un_vals, un_next_vals) = next_state(vals_and_commit);
+        assert!(verify_single_inner(
+            ts,
+            &un_sh,
+            &un_vals,
+            &un_next_vals,
+            TrustThresholdFraction::default()
+        )
+        .is_ok());
+    }
+
+    // valid to skip, but invalid commit. 1 validator.
+    #[test]
+    fn test_verify_single_skip_1_val_verify() {
+        let vac = ValsAndCommit::new(vec![0], vec![0]);
+        let ts = &init_trusted_state(vac, vec![0], 1);
+
+        // 100% overlap, but wrong commit.
+        // NOTE: This should be an invalid commit error since there's
+        // a vote from a validator not in the set!
+        // but voting_power_in isn't smart enough to see this ...
+        // TODO(ismail): https://github.com/interchainio/tendermint-rs/issues/140
+        let invalid_vac = ValsAndCommit::new(vec![1], vec![0]);
+        assert_single_err(
+            ts,
+            invalid_vac,
+            String::from(
+                "signed voting power (0) do not account for +2/3 of the total voting power: (1)",
+            ),
+        );
+    }
+
+    // valid commit and data, starting with 1 validator.
+    // test if we can skip to it.
+    #[test]
+    fn test_verify_single_skip_1_val_skip() {
+        let mut vac = ValsAndCommit::new(vec![0], vec![0]);
+        let ts = &init_trusted_state(vac.clone(), vec![0], 1);
+        //*****
+        // Ok
+
+        // 100% overlap (original signer is present in commit)
+        assert_single_ok(ts, vac);
+
+        vac = ValsAndCommit::new(vec![0, 1], vec![0, 1]);
+        assert_single_ok(ts, vac);
+
+        vac = ValsAndCommit::new(vec![0, 1, 2], vec![0, 1, 2]);
+        assert_single_ok(ts, vac);
+
+        vac = ValsAndCommit::new(vec![0, 1, 2, 3], vec![0, 1, 2, 3]);
+        assert_single_ok(ts, vac);
+
+        //*****
+        // Err
+        let err = "signed voting power (0) is too small fraction of total voting power: (1), threshold: TrustThresholdFraction { numerator: 1, denominator: 3 }";
+
+        // 0% overlap - new val set without the original signer
+        vac = ValsAndCommit::new(vec![1], vec![1]);
+        assert_single_err(ts, vac, err.clone().into());
+
+        // 0% overlap - val set contains original signer, but they didn't sign
+        vac = ValsAndCommit::new(vec![0, 1, 2, 3], vec![1, 2, 3]);
+        assert_single_err(ts, vac, err.into());
+    }
+
+    // valid commit and data, starting with 2 validators.
+    // test if we can skip to it.
+    #[test]
+    fn test_verify_single_skip_2_val_skip() {
+        let mut vac = ValsAndCommit::new(vec![0, 1], vec![0, 1]);
+        let ts = &init_trusted_state(vac.clone(), vec![0, 1], 1);
+
+        //*************
+        // OK
+
+        // 100% overlap (both original signers still present)
+        assert_single_ok(ts, vac);
+
+        vac = ValsAndCommit::new(vec![0, 1, 2], vec![0, 1, 2]);
+        assert_single_ok(ts, vac);
+
+        // 50% overlap (one original signer still present)
+        vac = ValsAndCommit::new(vec![0], vec![0]);
+        assert_single_ok(ts, vac);
+
+        vac = ValsAndCommit::new(vec![0, 1, 2, 3], vec![1, 2, 3]);
+        assert_single_ok(ts, vac);
+
+        //*************
+        // Err
+        let err = "signed voting power (0) is too small fraction of total voting power: (2), threshold: TrustThresholdFraction { numerator: 1, denominator: 3 }";
+
+        // 0% overlap (neither original signer still present)
+        vac = ValsAndCommit::new(vec![2], vec![2]);
+        assert_single_err(ts, vac, err.clone().into());
+
+        // 0% overlap (original signer is still in val set but not in commit)
+        vac = ValsAndCommit::new(vec![0, 2, 3, 4], vec![2, 3, 4]);
+        assert_single_err(ts, vac, err.into());
+    }
+
+    // valid commit and data, starting with 3 validators.
+    // test if we can skip to it.
+    #[test]
+    fn test_verify_single_skip_3_val_skip() {
+        let mut vac = ValsAndCommit::new(vec![0, 1, 2], vec![0, 1, 2]);
+        let ts = &init_trusted_state(vac.clone(), vec![0, 1, 2], 1);
+
+        //*************
+        // OK
+
+        // 100% overlap (both original signers still present)
+        assert_single_ok(ts, vac);
+
+        vac = ValsAndCommit::new(vec![0, 1, 2, 3], vec![0, 1, 2, 3]);
+        assert_single_ok(ts, vac);
+
+        // 66% overlap (two original signers still present)
+        vac = ValsAndCommit::new(vec![0, 1], vec![0, 1]);
+        assert_single_ok(ts, vac);
+
+        vac = ValsAndCommit::new(vec![0, 1, 2, 3], vec![1, 2, 3]);
+        assert_single_ok(ts, vac);
+
+        //*************
+        // Err
+        let err = "signed voting power (1) is too small fraction of total voting power: (3), threshold: TrustThresholdFraction { numerator: 1, denominator: 3 }";
+
+        // 33% overlap (one original signer still present)
+        vac = ValsAndCommit::new(vec![0], vec![0]);
+        assert_single_err(ts, vac, err.clone().into());
+
+        vac = ValsAndCommit::new(vec![0, 3], vec![0, 3]);
+        assert_single_err(ts, vac, err.clone().into());
+
+        // 0% overlap (neither original signer still present)
+        vac = ValsAndCommit::new(vec![3], vec![2]);
+        assert_single_err(ts, vac, err.into());
+
+        let err = "signed voting power (0) is too small fraction of total voting power: (3), threshold: TrustThresholdFraction { numerator: 1, denominator: 3 }";
+
+        // 0% overlap (original signer is still in val set but not in commit)
+        vac = ValsAndCommit::new(vec![0, 3, 4, 5], vec![3, 4, 5]);
+        assert_single_err(ts, vac, err.into());
+    }
+
+    #[test]
+    fn test_verify_single_skip_4_val_skip() {
+        let vac = ValsAndCommit::new(vec![0, 1, 2, 3], vec![0, 1, 2, 3]);
+        let ts = &init_trusted_state(vac.clone(), vec![0, 1, 2, 3], 1);
+
+        // 100% overlap (all signers present)
+        assert_single_ok(ts, vac);
+
+        // 75% overlap (three signers present)
+        let vac = ValsAndCommit::new(vec![0, 1, 2], vec![0, 1, 2]);
+        assert_single_ok(ts, vac);
+
+        let vac = ValsAndCommit::new(vec![0, 1, 2, 4], vec![0, 1, 2, 4]);
+        assert_single_ok(ts, vac);
+
+        // 50% overlap (two signers still present)
+        let vac = ValsAndCommit::new(vec![0, 1], vec![0, 1]);
+        assert_single_ok(ts, vac);
+
+        let vac = ValsAndCommit::new(vec![0, 1, 4, 5], vec![0, 1, 4, 5]);
+        assert_single_ok(ts, vac);
+
+        let err = "signed voting power (1) is too small fraction of total voting power: (4), threshold: TrustThresholdFraction { numerator: 1, denominator: 3 }";
+
+        // 25% overlap (one signer still present)
+        let vac = ValsAndCommit::new(vec![0, 4, 5, 6], vec![0, 4, 5, 6]);
+        assert_single_err(ts, vac, err.into());
+
+        let err = "signed voting power (0) is too small fraction of total voting power: (4), threshold: TrustThresholdFraction { numerator: 1, denominator: 3 }";
+
+        // 0% overlap (none of the signers present)
+        let vac = ValsAndCommit::new(vec![4, 5, 6], vec![4, 5, 6]);
+        assert_single_err(ts, vac, err.clone().into());
+
+        // 0% overlap (one signer present in val set but does not commit)
+        let vac = ValsAndCommit::new(vec![3, 4, 5, 6], vec![4, 5, 6]);
+        assert_single_err(ts, vac, err.into());
+    }
+
+    #[test]
+    fn test_is_within_trust_period() {
+        let header_time = SystemTime::UNIX_EPOCH;
+        let period = Duration::new(100, 0);
+        let now = header_time + Duration::new(10, 0);
+
+        // less than the period, OK
+        let header = MockHeader::new(4, header_time, fixed_hash(), fixed_hash());
+        assert!(is_within_trust_period(&header, period, now).is_ok());
+
+        // equal to the period, not OK
+        let now = header_time + period;
+        assert!(is_within_trust_period(&header, period, now).is_err());
+
+        // greater than the period, not OK
+        let now = header_time + period + Duration::new(1, 0);
+        assert!(is_within_trust_period(&header, period, now).is_err());
+
+        // bft time in header is later than now, not OK:
+        let now = SystemTime::UNIX_EPOCH;
+        let later_than_now = now + Duration::new(60, 0);
+        let future_header = MockHeader::new(4, later_than_now, fixed_hash(), fixed_hash());
+        assert!(is_within_trust_period(&future_header, period, now).is_err());
+    }
+}
